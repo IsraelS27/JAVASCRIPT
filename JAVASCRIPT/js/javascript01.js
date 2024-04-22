7999c6148cde525848c242905d747e3dc99fb8a2
IsraelS27 <152341651+IsraelS27@users.noreply.github.com>
2024-04-22 09:28:07 -0400

    Add files via upload

---
+++ JAVASCRIPT/js/javascript01.js
@@ -0,0 +1,9 @@
+/*
+Descripcion: Ejemplo de Cadena por console.log
+Desarrollador: Israel Simone Justiniano
+Fecha: 2024-04-15
+Cambios: Ninguno
+*/
+
+var mensaje="Imprimir en consola";
+console.log(mensaje);
